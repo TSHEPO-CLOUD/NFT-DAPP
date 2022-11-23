df25ddfab2acb8fafa08e9c3ae1901836cda7ae0
tshepo <tshepomolefeda@gmail.com>
2022-11-23 13:30:28 +0200

    Deploy to resrnet

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -4,13 +4,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract GaneItem is ERC721 {
+contract GameItem is ERC721 {
 
-    constructor()  ERC721("GaneItem", "ITM") {
-        -mint(msg.sender,1);
-        -mint(msg.sender,2);
-        -mint(msg.sender,3);
-        -mint(msg.sender,4);
-        -mint(msg.sender,5);
+    constructor()  ERC721("GameItem", "ITM") {
+        _mint(msg.sender,1);
+       
     }
 }
